09603d1a8de23da96020bfad09cf8ac9649a4c66
Cuong Bao <cuong.bao@asiantech.vn>
2018-11-23 14:35:51 -0800

    Change algorithm

--- js/exercise.js
+++ js/exercise.js
@@ -1,5 +1,5 @@
 // Question 1
-let sum = function (a, b) {
+let sum = function (a = 0, b = 0) {
   if (+a == +b) {
     return a * 6;
   }
@@ -7,7 +7,7 @@ let sum = function (a, b) {
 };
 document.getElementById('question_1').innerHTML = sum(5, 5);
 // Question 2
-let compare = function (a) {
+let compare = function (a = 0) {
   if (a > 19) {
     return (a - 19) * 3;
   }
@@ -15,15 +15,30 @@ let compare = function (a) {
 };
 document.getElementById('question_2').innerHTML = compare(20);
 // Question 3, question 4
-let divisibleByNumber = function (inp, num) {
-  let result = [], newArg;
-  for (let i = 0; i <= 9; i++) {
-    newArg = +inp.replace("*", i);
-    if (+newArg % num == 0) {
-      result.push(newArg);
+let divisibleByNumber = function(inp, num = 3) {
+  let result = [], sum = 0;
+  if (inp.length) {
+    for (let i = 0; i < inp.length; i++) {
+      if (inp[i] === "*") {
+        continue;
+      } 
+      sum += +inp[i];
+    }
+    for (let i = 0; i <= 9; i++) {
+      if ((sum + i) % num == 0) {
+        result.push(inp.replace("*", i));
+      }
+    }
+    if (num == 6) {
+      for (let i = result.length - 1; i >= 0; i--) {
+        let resultTemp = result[i];
+        if(resultTemp.charAt(resultTemp.length - 1) % 2 != 0) {
+          result.splice(i, 1);
+        }
+      }
     }
   }
   return result;
-};
-document.getElementById('question_3').innerHTML = divisibleByNumber("234*", 3);
+}
+document.getElementById('question_3').innerHTML = divisibleByNumber("234*");
 document.getElementById('question_4').innerHTML = divisibleByNumber("1234567890*", 6);

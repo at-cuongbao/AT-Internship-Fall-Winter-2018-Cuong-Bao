9c792e796235a7abd902bca6cb1793d43918e67c
Cuong Bao <cuong.bao@asiantech.vn>
2018-11-23 09:02:48 -0800

    23/11/2018 Update Javascript homework

--- js/exercise.js
+++ js/exercise.js
@@ -1,40 +1,29 @@
 // Question 1
-var sum = function (a, b) {
-  if (a === b) {
-    return a * 3;
+let sum = function (a, b) {
+  if (+a == +b) {
+    return a * 6;
   }
   return a + b;
 };
 document.getElementById('question_1').innerHTML = sum(5, 5);
 // Question 2
-var compare = function (a) {
+let compare = function (a) {
   if (a > 19) {
     return (a - 19) * 3;
   }
   return (19 - a); 
 };
 document.getElementById('question_2').innerHTML = compare(20);
-// Question 3
-let divisibleByThree = function (arg) {
-  let result = [];
+// Question 3, question 4
+let divisibleByNumber = function (inp, num) {
+  let result = [], newArg;
   for (let i = 0; i <= 9; i++) {
-    let newArg = +arg.replace("*", i);
-    if (+newArg % 3 == 0) {
-      result.push('[' + newArg + ']');
+    newArg = +inp.replace("*", i);
+    if (+newArg % num == 0) {
+      result.push(newArg);
     }
   }
   return result;
 };
-document.getElementById('question_3').innerHTML = divisibleByThree("234*");
-// Question 4
-let divisibleBySix = function (arg) {
-  let result = [];
-  for (let i = 0; i <= 9; i++) {
-    let newArg = +arg.replace("*", i);
-    if (+newArg % 6 == 0) {
-      result.push('[' + newArg + ']');
-    }
-  }
-  return result;
-};
-document.getElementById('question_4').innerHTML = divisibleBySix("*66");
+document.getElementById('question_3').innerHTML = divisibleByNumber("234*", 3);
+document.getElementById('question_4').innerHTML = divisibleByNumber("1234567890*", 6);
